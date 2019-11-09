78cce73f57ad66031c8e68737cc0c8d1903615e8
Faxriddin Masharipov <faxriddin5957@gmail.com>
2019-11-09 12:40:23 +0500

    title 3

--- frontend/views/site/nokboss.php
+++ frontend/views/site/nokboss.php
@@ -5,7 +5,7 @@ use frontend\models\Asos;
 use frontend\models\AsosSlave;
 use frontend\models\User;
 use kartik\date\DatePicker;
-$this->title="2";
+$this->title="3";
 $date = date('Y-m-d');
 ?>
 <div class="client-qarz">
